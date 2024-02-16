ec50c1dcbd7e0de34e86640a9d94b23bc45ed5c0
Alex <muchenealex24@gmail.com>
2024-02-16 10:04:10 +0300

    add redux and redux persist

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,7 +1,125 @@
-import React from "react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+
+  const {
+    currentUser,
+    error: errorMessage,
+    loading,
+  } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  console.log(formData);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(signInStart());
+
+    try {
+      if (!email || !password) {
+        dispatch(signInFailure("All fields are required"));
+        return;
+      }
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  };
+  return (
+    <div className="min-h-screen mt-20 p-5">
+      <div className="flex max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5 ">
+        {/*right side*/}
+        <div className="flex-1">
+          <Link
+            to={"/"}
+            className="text-4xl dark:text-white sm:text-4xl font-bold"
+          >
+            <span className="px-2 py-1 rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
+              Muchene's
+            </span>
+            blog
+          </Link>
+          <p className="mt-5 text:sm">
+            Sign in with your email and password or continue with google.{" "}
+          </p>
+        </div>
+        {/*left side */}
+        <div className="flex-1">
+          <form onSubmit={handleSubmit} className="flex-col gap-3 flex">
+            <div>
+              <Label value="Email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Password" />
+              <TextInput
+                type="password"
+                placeholder="********"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don't have an acount?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && <Alert color="failure">{errorMessage}</Alert>}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
